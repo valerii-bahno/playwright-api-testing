bd8aa9ea0a9d291ff03115940921cb2f5ecf55a3
valeriibahno <valeriybagno89@gmail.com>
2025-05-12 22:37:41 +0300

    Clean up

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,6 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     trace: 'retain-on-failure',
-    headless: true,
     ignoreHTTPSErrors: true,
     extraHTTPHeaders: {
       'Authorization': `Token ${process.env.ACCESS_TOKEN}`
