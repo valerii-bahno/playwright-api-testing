da59791ca72806a17eb08a3f3201680c1b11fc70
valeriibahno <valeriybagno89@gmail.com>
2025-05-13 21:03:10 +0300

    Fixed

--- global-setup.ts
+++ global-setup.ts
@@ -1,38 +1,3 @@
-// import { request, expect } from '@playwright/test';
-
-// async function globalSetup() {
-//     const context = await request.newContext();
-
-//     const responseToken = await context.post('https://conduit-api.bondaracademy.com/api/users/login', {
-//         data: { 
-//           "user": {"email": process.env.LOGIN_USER, "password": process.env.LOGIN_PASSWORD}
-//         }
-//       });
-    
-//     const responseBody = await responseToken.json();
-//     process.env['ACCESS_TOKEN'] = await responseBody.user.token;
-
-//     const cookieSession = './.auth/userSession.json';
-
-//     await context.storageState({ path: cookieSession });
-
-//     const articleResponse = await context.post('https://conduit-api.bondaracademy.com/api/articles/', {
-//             data: {
-//               "article":{"title":"Global Likes test article","description":"This is a global test description","body":"This is a global body of new article","tagList":[]}
-//             },
-//             headers: {
-//                 Authorization: `Token ${process.env.ACCESS_TOKEN}`
-//             }
-//           });
-        
-//     expect(articleResponse.status()).toEqual(201);
-    
-//     const response = await articleResponse.json();
-//     process.env['SLUGID'] = await response.article.slug;
-// }
-
-// export default globalSetup;
-
 import { request, expect, FullConfig, chromium } from '@playwright/test';
 
 const authFile = '.auth/userSession.json';
@@ -46,7 +11,7 @@ async function globalSetup(config: FullConfig) {
 
     const responseToken = await contextApi.post('https://conduit-api.bondaracademy.com/api/users/login', {
         data: { 
-          "user": {"email": "testrbvaleriibahno@gmail.com", "password": "Valerii_test123!"}
+          "user": {"email": process.env.LOGIN_USER, "password": process.env.LOGIN_PASSWORD}
         }
       });
     
--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -4,29 +4,7 @@ import fs from 'fs';
 
 const authFile = './.auth/userSession.json';
 
-setup('authentication', async ({ context, request }) => {
-    // const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
-    //     data: { 
-    //       "user": {"email": process.env.LOGIN_USER, "password": process.env.LOGIN_PASSWORD}
-    //     }
-    //   });
-    
-    // const responseBody = await response.json();
-
-    // // user.origins[0].localStorage[0].value = responseBody.user.token;
-    // fs.writeFileSync(authFile, JSON.stringify(user));
-    // // fs.writeFileSync(authFile, JSON.stringify({
-    // //   // token: responseBody.user.token
-    // //   token: user
-    // // }));
-
-    // const cookieSession = './.auth/userSession.json';
-
-    // await context.storageState({ path: cookieSession });
-
-    // process.env['ACCESS_TOKEN'] = responseBody.user.token;
-
-
+setup('authentication', async ({ request }) => {
     const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
         data: { 
           "user": {"email": "testV123456@test.com", "password": "test123!"}
@@ -36,8 +14,9 @@ setup('authentication', async ({ context, request }) => {
     const responseBody = await response.json();
     const accessToken = responseBody.user.token;
 
-    user.origins[0].localStorage[0].value = accessToken;
-    fs.writeFileSync(authFile, JSON.stringify(user));
+    // user.origins[0].localStorage[0].value = accessToken;
+    // fs.writeFileSync(authFile, JSON.stringify(user));
 
-    process.env['ACCESS_TOKEN'] = accessToken;
+    // process.env['ACCESS_TOKEN'] = accessToken;
+    process.env['ACCESS_TOKEN'] = responseBody.user.token;
 });
